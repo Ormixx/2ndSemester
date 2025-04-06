184a584626388f5c53f2b82ebc577856061f201c
Пользователь <razanovaleksej153@gmail.com>
2025-04-06 12:08:29 +0500

    Выпоолнение задания.

---
+++ src/LaboratoryWork4/LaboratoryWork4/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoryWork4
+{
+    public class Item
+    {
+        public int Key { get; }
+
+        public Item(int key)
+        {
+            Key = key;
+        }
+    }
+}
---
+++ src/LaboratoryWork4/LaboratoryWork4/Program.cs
@@ -0,0 +1,43 @@
+﻿using LaboratoryWork4;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        HashTable hashTable = new HashTable(10);
+
+        hashTable.Insert(new Item(1));
+        hashTable.Insert(new Item(2));
+        hashTable.Insert(new Item(3));
+        hashTable.Insert(new Item(12));
+        hashTable.Insert(new Item(22));
+
+        System.Console.WriteLine("Содержимое хеш-таблицы:");
+        hashTable.DisplayHashTable();
+
+        int searchKey = 12;
+        Item foundItem = hashTable.Find(searchKey);
+        if (foundItem != null)
+        {
+            System.Console.WriteLine($"Элемент с ключом {searchKey} найден.");
+        }
+        else
+        {
+            System.Console.WriteLine($"Элемент с ключом {searchKey} не найден.");
+        }
+
+        int deleteKey = 2;
+        Item deletedItem = hashTable.Delete(deleteKey);
+        if (deletedItem != null)
+        {
+            System.Console.WriteLine($"Элемент с ключом {deleteKey} удален.");
+        }
+        else
+        {
+            System.Console.WriteLine($"Элемент с ключом {deleteKey} не найден для удаления.");
+        }
+
+        System.Console.WriteLine("Содержимое хеш-таблицы после удаления:");
+        hashTable.DisplayHashTable();
+    }
+}

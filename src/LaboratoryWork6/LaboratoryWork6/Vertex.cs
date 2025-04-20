f07a4249b6314ae01b61130e3994ee70ae48901f
Пользователь <razanovaleksej153@gmail.com>
2025-04-20 11:06:24 +0500

    Выполнение задания.

---
+++ src/LaboratoryWork6/LaboratoryWork6/Vertex.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoryWork6
+{
+    public class Vertex
+    {
+        public char Label { get; private set; }
+        public List<Vertex> AdjacentVertices { get; private set; }
+
+        public Vertex(char label)
+        {
+            Label = label;
+            AdjacentVertices = new List<Vertex>();
+        }
+
+        public void AddAdjacent(Vertex vertex)
+        {
+            AdjacentVertices.Add(vertex);
+        }
+    }
+}
